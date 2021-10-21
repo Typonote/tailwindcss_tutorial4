8d5e9bd3e33eb1c8f5a826e670c6d47ecfb24a0a
Typonote <kimradium@naver.com>
2021-10-21 23:56:22 +0900

    #7 내용 삽입

--- src/data/dummy.js
+++ src/data/dummy.js
@@ -48,7 +48,7 @@ const dummy = {
                     alt: 'android studio',
                     },
             ],
-            desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only`,
+            desc: `저는 react에 관심이 많습니다.`,
         },
 };
 
