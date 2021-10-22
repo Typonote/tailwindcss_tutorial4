520cf27c3dfc9e5b119f5cbcc2a7c3a5acc6559f
Typonote <kimradium@naver.com>
2021-10-22 18:43:45 +0900

    #9 projects 내용 변경

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import image1 from '../assets/teamproject1.png'
 
 const Projects = () => {
     return (
@@ -8,42 +10,50 @@ const Projects = () => {
 
         <div class="grid xl:grid-cols-4 md:grid-cols-2 sm:grid-col-1 px-8 py-24 gap-8">
 
-            <div >
-                <div>
-                    <div class="bg-white p-6 rounded-lg">
-                        <img class="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72  rounded w-full object-cover object-center mb-6" src="https://kuyou.id/content/images/ctc_2020021605150668915.jpg" alt="Image Size 720x400" />
-                        <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-                        <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Chichen Itza</h2>
-                        <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-                    </div>
-                </div>
-            </div>
             
             <div>
-                <div class="bg-white p-6 rounded-lg">
-                    <img class="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72 rounded w-full object-cover object-center mb-6" src="https://asset.kompas.com/crops/Pk_pN6vllxXy1RshYsEv74Q1BYA=/56x0:1553x998/750x500/data/photo/2021/06/16/60c8f9d68ff4a.jpg" alt="Image Size 720x400" />
-                    <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-                    <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Colosseum Roma</h2>
-                    <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-                </div>
+                <a href='https://github.com/Typonote/ReviewKingProject' >
+                    <div class="bg-white p-6 rounded-lg border-2 border-white hover:bg-gray-100   hover:border-gray-600">
+                        <img class="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72  rounded w-full object-cover object-center mb-6" src={image1} alt="Image Size 720x400" />
+                        <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font font-bold mb-3">팀 프로젝트</h3>
+                        <h2 class="text-lg text-gray-900 font-medium title-font mb-1 font-bold">음식점 리뷰 사이트 제작</h2>
+                        <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font font-bold mb-4">프론트엔드</h3>                        <p class="leading-relaxed text-base">크롤링한 음식점 리뷰 데이터를 워드 클라우드로 변환하여 소비자가 한눈에 볼 수 있는 사이트를 제작하였습니다.</p>
+                    </div>
+                </a>
+            </div>
+            
+            
+            <div>
+                <a href='https://github.com/Typonote/ReviewKingProject' >
+                    <div class="bg-white p-6 rounded-lg border-2 border-white hover:bg-gray-100   hover:border-gray-600">
+                        <img class="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72  rounded w-full object-cover object-center mb-6" src={image1} alt="Image Size 720x400" />
+                        <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font font-bold mb-3">팀 프로젝트</h3>
+                        <h2 class="text-lg text-gray-900 font-medium title-font mb-1 font-bold">음식점 리뷰 사이트 제작</h2>
+                        <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font font-bold mb-4">프론트엔드</h3>                        <p class="leading-relaxed text-base">크롤링한 음식점 리뷰 데이터를 워드 클라우드로 변환하여 소비자가 한눈에 볼 수 있는 사이트를 제작하였습니다.</p>
+                    </div>
+                </a>
             </div>
 
             <div>
-                <div class="bg-white p-6 rounded-lg">
-                    <img class="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72 rounded w-full object-cover object-center mb-6" src="https://images.immediate.co.uk/production/volatile/sites/7/2019/07/33-GettyImages-154260931-216706f.jpg?quality=90&resize=768%2C574" alt="Image Size 720x400"/>
-                    <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-                    <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Great Pyramid of Giza</h2>
-                    <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-                </div>
+                <a href='https://github.com/Typonote/ReviewKingProject' >
+                    <div class="bg-white p-6 rounded-lg border-2 border-white hover:bg-gray-100   hover:border-gray-600">
+                        <img class="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72  rounded w-full object-cover object-center mb-6" src={image1} alt="Image Size 720x400" />
+                        <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font font-bold mb-3">팀 프로젝트</h3>
+                        <h2 class="text-lg text-gray-900 font-medium title-font mb-1 font-bold">음식점 리뷰 사이트 제작</h2>
+                        <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font font-bold mb-4">프론트엔드</h3>                        <p class="leading-relaxed text-base">크롤링한 음식점 리뷰 데이터를 워드 클라우드로 변환하여 소비자가 한눈에 볼 수 있는 사이트를 제작하였습니다.</p>
+                    </div>
+                </a>
             </div>
 
             <div>
-                <div class="bg-white p-6 rounded-lg">
-                    <img class="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72 rounded w-full object-cover object-center mb-6" src="https://wisatamuda.com/wp-content/uploads/2019/02/1-Golden-Gate-Bridge-Gambar-dan-Foto-Tempat-Wisata-Terbaik-di-San-Fransisco-USA.jpg" alt="Image Size 720x400"/>
-                    <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-                    <h2 class="text-lg text-gray-900 font-medium title-font mb-4">San Francisco</h2>
-                    <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-                </div>
+                <a href='https://github.com/Typonote/ReviewKingProject' >
+                    <div class="bg-white p-6 rounded-lg border-2 border-white hover:bg-gray-100   hover:border-gray-600">
+                        <img class="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72  rounded w-full object-cover object-center mb-6" src={image1} alt="Image Size 720x400" />
+                        <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font font-bold mb-3">팀 프로젝트</h3>
+                        <h2 class="text-lg text-gray-900 font-medium title-font mb-1 font-bold">음식점 리뷰 사이트 제작</h2>
+                        <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font font-bold mb-4">프론트엔드</h3>                        <p class="leading-relaxed text-base">크롤링한 음식점 리뷰 데이터를 워드 클라우드로 변환하여 소비자가 한눈에 볼 수 있는 사이트를 제작하였습니다.</p>
+                    </div>
+                </a>
             </div>
 
         </div>
